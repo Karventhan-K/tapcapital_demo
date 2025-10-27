4ad8befe4e0bf4d529ef31e63d0a0c7bac518ab2
Karventhank <karventhan@Karventhan.localdomain>
2025-10-27 09:03:54 +0000

    port change

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
 	return (
 		<Html lang="en">
 			<Head >
-				<script
-					src="https://widget.agentzee.ai/chat/widgets.js?agentId=4a065de0-58ce-4d33-9f0d-d28187d764cd"
-					async
-					></script>
+				<script src="https://widget.agentzee.ai/chat/widgets.js?agentId=4a065de0-58ce-4d33-9f0d-d28187d764cd"></script>
 			</Head>
 			<body className="antialiased">
 				<Main />
