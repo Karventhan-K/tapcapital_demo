0a7d8730227f3fbf02487d5d23bfebd4ae7cdbaa
Karventhank <karventhan@Karventhan.localdomain>
2025-10-27 08:45:24 +0000

    port change

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,16 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+	return (
+		<Html lang="en">
+			<Head >
+				<Script src="https://widget.agentzee.ai/chat/widgets.js?agentId=4a065de0-58ce-4d33-9f0d-d28187d764cd"></Script >
+			</Head>
+			<body className="antialiased">
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
